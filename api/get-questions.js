1617604a3781f319803170d83f25c697514d1102
El sosox <pxricosidad@gmail.com>
2025-07-11 09:40:26 -0500

    Update get-questions.js

--- api/get-questions.js
+++ api/get-questions.js
@@ -1,6 +1,6 @@
 import { OpenAI } from 'openai';
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const openai = new OpenAI({ apiKey: process.env.API_KEY });
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
